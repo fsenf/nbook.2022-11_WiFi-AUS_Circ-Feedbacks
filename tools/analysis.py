a16533971aa0725447805d254db4742ba2251cf5
Fabian Senf <senf@tropos.de>
2022-12-11 14:32:24 +0100

    added circ analysis to tools

--- tools/analysis.py
+++ tools/analysis.py
@@ -6,34 +6,290 @@ import xarray as xr
 xr.set_options(keep_attrs=True)
 
 ######################################################################
+# Constants ----------------------------------------------------------
 ######################################################################
 
-def calc_stream_function( dset ):
+# from mo_physical_constants.f90:90:  REAL(wp), PARAMETER :: earth_radius           = 6.371229e6_wp    !! [m]    average radius
+earth_radius      = 6.371229e6
+a = earth_radius
+g = 9.81
+day = 24*3600.
+Omega = 2 * np.pi / day
+
+
+######################################################################
+# Circulation Analysis  ----------------------------------------------
+######################################################################
+
+def calc_stream_function( v, dset, method = 'p-coordinate', invert_p = True):
 
     
-    # get height increments
-    full_height, half_height = reader.get_mean_geoptential_height( averaging_dims = ('time', 'lon') )
-    
-    dz = np.abs( half_height.diff('lev') ) * 1e3   # units: m
-    dz = dz.assign_coords({'lev': full_height})
-
-    rhov = uv['rhov'].mean(['time',])
-    
-    # integration
-    kernel = rhov * dz
-    kernel = kernel.reindex( lev = np.sort(dz.lev) )
-    integral = kernel.cumsum('lev')
-
-    phi = np.deg2rad( kernel.lat )
+    # define constants and coordinates
+    phi = np.deg2rad( v.lat ) 
     cosphi = np.cos( phi )
-    a = 6372e3    # units: m
+    
+    if method == 'z-coordinate':   # maybe broken ...
+        
+        # get height increments
+        full_height, half_height = reader.get_mean_geoptential_height( averaging_dims = ('time', 'lon') )
 
-    Psi = 2*np.pi*a*cosphi * integral / 1e10   # units 1e10 kg s-1
+        dz = np.abs( half_height.diff('lev') ) * 1e3   # units: m
+        dz = dz.assign_coords({'lev': full_height})
+
+        rhov = v['rhov'].mean(['time',])
+
+        # integration
+        kernel = rhov * dz
+        kernel = kernel.reindex( lev = np.sort(dz.lev) )
+        integral = kernel.cumsum('lev')
+        
+    elif method == 'p-coordinate':
+        
+        pressure_mask = dset['aps'] > dset.plev
+
+        # mask meridional wind and sort in descending order
+        vm = v.where( pressure_mask, 0 )
+    
+        if invert_p:
+            vm_re = vm.reindex(plev=np.sort( vm.plev.values)[::-1])
+        else:
+            vm_re = vm
+
+        # calculate the vertical integral
+        fac = - 1 / g
+        integral = fac * vm_re.cumulative_integrate('plev').where( pressure_mask ) # .sel ( time = '2020-01').mean('time')
+        
+
+    Psi = 2*np.pi*a*cosphi * integral # ReRun_Mean_Circulation_ResponseReRun_Mean_Circulation_Response/ 1e10   # units 1e10 kg s-1
 
     return Psi
 
 ######################################################################
 ######################################################################
+
+def calc_vstar( dset ):
+    
+    '''
+    Parameters
+    ----------
+    dset : xarray.Dataset
+        input dataset containing meanflow vars and covariances 
+        stored at pressure levels
+        
+        
+    Returns
+    -------
+    v_star : xarray.Dataset
+        residual meridional velocity
+        
+    
+    Notes
+    -----
+    Following calculations are done:
+    
+    $$
+    v^* = \overline{v} - 
+    \partial_p\bigg(  
+    \frac{\;\overline{v`\theta`}}
+    {\partial_p\overline{\theta}} \bigg) 
+    = \overline{v} + v_\mathrm{eddy}
+    $$
+    '''
+    
+    # mean vars (w.r.t to zonal average)
+    v = dset['v']
+    theta = dset['tpot']
+
+    # get stablity
+    Gamma = theta.differentiate('plev')
+    
+    # calculate covariance
+    F_vtheta = dset['vtheta'] - v * theta
+
+    # combine mean and eddy components
+    v_eddy = - ( F_vtheta / Gamma ).differentiate( 'plev' )
+    v_star = v + v_eddy
+    
+    return v_star
+
+    
+    
+######################################################################
+######################################################################
+
+def horizontal_divergence( f, latitude_name = 'lat', exponent = 1 ):
+    
+    '''
+    Calculates horizontal divergence (only meridional direction) of a flux f.
+    
+    
+    Parameters
+    ----------
+    f : xr.DataArray
+        input flux
+    
+
+    Returns
+    -------
+    div_f : xr.DataArray
+        divergance of flux f in spherical coordinates
+        
+        
+    Notes
+    -----
+    Metric correction of the generalized vertical coordinate are ignored.
+    '''
+    
+    
+    # latitudes
+    phi = np.deg2rad( f[latitude_name] )
+    cosphi = np.cos( phi )**exponent
+
+
+    
+    # derivatives
+    d_cosphi_f  = (cosphi * f).differentiate( latitude_name )
+    dphi        = (phi).differentiate( latitude_name )
+    
+    return 1 / (a * cosphi) * d_cosphi_f / dphi
+
+
+######################################################################
+######################################################################
+
+
+def calculate_eddy_omega( d, level_name = 'plev'  ):
+
+   
+
+    # calculate heat flux
+    v = d['v']
+    theta = d['tpot']
+
+    # get stablity
+    
+    # calculate covariance
+    F_vtheta = d['vtheta'] - v * theta
+
+    # and its divergence
+    div_F_vtheta = horizontal_divergence( F_vtheta )
+
+
+    # calculate stability
+    Gamma = theta.differentiate('plev')
+
+    # put all together
+    omega_e = div_F_vtheta / Gamma
+
+
+    return omega_e
+
+    
+######################################################################
+######################################################################
+
+
+def calculate_residual_omega( d, level_name = 'plev' ):
+
+    '''
+    Notes
+    -----
+    This formula is implemented
+    $$
+    \omega^* = \overline{\omega} + 
+    \frac{1}{a\cos\varphi}
+    \frac{\partial_\varphi\big( \cos\varphi \;\overline{v`\theta`} \big) }
+    {\partial_p\overline{\theta}}
+    = \overline{\omega} + \omega_\mathrm{eddy}
+    $$
+    '''
+    
+    # get vertical velocity in pressure coordinates
+    omega_bar = d['omega']
+
+    # calculate omega components
+    omega_eddy = calculate_eddy_omega( d, 
+                                      level_name = level_name )
+
+
+    omega_star = omega_bar + omega_eddy
+
+    return omega_star
+
+######################################################################
+######################################################################
+
+def calc_meridional_EPF( d,  ):
+
+   
+    # define coordinates
+    phi = np.deg2rad( d.lat ) 
+    cosphi = np.cos( phi )
+
+    # calculate momentum flux
+    u = d['u']
+    v = d['v']
+    
+    
+    # calculate covariance
+    F_uv = d['uv'] - v * u
+
+
+    EPF_phi = -a * cosphi * F_uv 
+    
+    return EPF_phi
+
+######################################################################
+######################################################################
+
+def calc_vertical_EPF( d,  ):
+   
+    # define coordinates
+    phi = np.deg2rad( d.lat ) 
+    cosphi = np.cos( phi )
+    sinphi = np.sin( phi )
+
+    # calculate heat flux
+    v = d['v']
+    theta = d['tpot']
+   
+    # calculate covariance
+    F_vtheta = d['vtheta'] - v * theta
+    
+    # calculate stability
+    Gamma = theta.differentiate('plev')
+    
+    f = 2 * Omega * sinphi
+    
+    EPF_p = a * f * cosphi * F_vtheta / Gamma 
+    
+    return EPF_p
+    
+######################################################################
+######################################################################
+
+def EPF_divergence( d,  ):
+   
+    # define coordinates
+    phi = np.deg2rad( d.lat ) 
+    cosphi = np.cos( phi )
+
+    # EPF components
+    EPF_phi = calc_meridional_EPF( d,  )
+    EPF_p   = calc_vertical_EPF( d,  )
+    
+    div_EPF = horizontal_divergence( EPF_phi ) + EPF_p.differentiate('plev')
+    
+    return div_EPF
+    
+######################################################################
+######################################################################
+
+
+######################################################################
+# Statistics and Averaging -------------------------------------------
+######################################################################
+
 
 def ens_stat( vin ):
 
